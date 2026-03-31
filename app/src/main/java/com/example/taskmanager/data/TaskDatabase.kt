473902011da08d1e2c0494779893648931832ea5
Matheus2037 <matheusdasilvagastaldi@gmail.com>
2026-03-31 10:04:52 -0300

    Teste

--- app/src/main/java/com/example/taskmanager/data/TaskDatabase.kt
+++ app/src/main/java/com/example/taskmanager/data/TaskDatabase.kt
@@ -5,6 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
+//Database aplicado em modelo de Singleton
 @Database(entities = [TaskEntity::class], version = 1)
 abstract class TaskDatabase : RoomDatabase() {
 
